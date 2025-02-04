d53b06fe45623f639450140f908e18f137cad59b
Prasoon76823 <prasoongl1113@gmail.com>
2025-02-04 12:25:06 +0530

    Update pd7.c

--- pd7.c
+++ pd7.c
@@ -88,36 +88,38 @@
 
 // }
 
-void main()
-{
-    int a[10],i,n,count=0,ans;
-    printf("Enter size: ");
-    scanf("%d",&n);
-    printf("Entre array: ");
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    // for(i=0;i<n;i++)
-    // {
-    //     ans=ans^a[i];
-    // }
-    // printf("%d",ans);
+// void main()
+// {
+//     int a[10],i,n,count=0,ans;
+//     printf("Enter size: ");
+//     scanf("%d",&n);
+//     printf("Entre array: ");
+//     for(i=0;i<n;i++)
+//     {
+//         scanf("%d",&a[i]);
+//     }
+//************************using ^gate*********************************
+//     for(i=0;i<n;i++)
+//     {
+//         ans=ans^a[i];
+//     }
+//     printf("%d",ans);
+//***********************using loop***********************************
+//     for(i=0;i<n;i++)
+//     {
+//         count=0;
+//         for(int j=0;j<i;j++)
+//         {
+//             if(a[i]==a[j])
+//             {
+//                 count++;
+//             }
+//         }
+//         if(count%2!=0)
+//         {
+//             printf("%d",a[i]);
+//             break;
+//         }
+//     }
+// }
 
-    for(i=0;i<n;i++)
-    {
-        count=0;
-        for(int j=0;j<i;j++)
-        {
-            if(a[i]==a[j])
-            {
-                count++;
-            }
-        }
-        if(count%2!=0)
-        {
-            printf("%d",a[i]);
-            break;
-        }
-    }
-}
